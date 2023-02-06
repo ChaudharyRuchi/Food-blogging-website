d017f55758def2370d45efdb2814d7ee62c60de0
Ruchi Chaudhary <chaudharyruchi844@gmail.com>
2023-02-06 12:39:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+
+import './App.css';
+
+function App() {
+
+  
+  return (
+    <>
+    
+
+    <div className="header">
+      <img src="images\Screenshot_669 1.png" className="car"></img>
+      <img src="images\Discover the Best Food and Drinks.png" className="text"></img>
+      <p className="header-para">Naturally made Healthcare Products for the better care & support of your body</p>
+      <button className="header-button">Explore Now</button>
+      <img src="images\Rectangle 400.png" className="pizza-picture"></img>
+      <img src="images\Vector 1.png" className="vector"></img>
+     </div>
+
+
+     
+      <div className="semi-header-1"> 
+      <img src="images\pharmasict-serving-customer-drug-store 1.png" className="pharmasict"></img>
+      
+      <div className="semi-header-2">
+      <p className="details">About us</p>
+      <p className="details-para">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. t has survived not only five centuries.</p>
+      <button type="" className="button">Read more</button>
+        
+      
+      
+      </div>
+      </div>
+        <div className="Middle">
+          <h1 className="articles">Latest Articles</h1>
+          </div>
+
+          <div className="semi-container">
+            <img src="images\grilled-tomatoes-1-846x846 1 (2).png" ></img>
+            <p className="para">Grilled Tomatoes at Home</p>
+            <p className="loreum-para">PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...</p>
+            <button className="para-button">Read More</button>
+          </div>
+
+          <div className="box-2">
+          <img src="images\meal-prep-ideas-846x846 1 (4).png" className="meal-prep"></img>
+          <p className="para">Snacks for travel</p>
+          <p className="loreum-para">PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...</p>
+          <button className="para-button">Read More</button>
+          </div>
+
+          <div className="box-3">
+          <img src="images\meal-prep-ideas-846x846 1 (5).png" ></img>
+          <p className="para">Post-workout Recipes</p>
+          <p className="loreum-para">PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...</p>
+          <button className="para-button">Read More</button>
+          </div>
+          <div className="semi-container">
+            <img src="images\grilled-tomatoes-1-846x846 1 (3).png" ></img>
+            <p className="para">Grilled Tomatoes at Home</p>
+            <p className="loreum-para">PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...</p>
+            <button className="para-button">Read More</button>
+          </div>
+
+        
+          <div className="contact-details">
+            
+            <img src="images\Screenshot_669 1.png" className="footer-car"></img>
+            <h3 className='contact'>Contact us</h3>
+            <p className='contact-para-1'>Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM Market, XYZ-343434</p>
+            <p className='contact-para-2'>example2020@gmail.com</p>
+            <p className='contact-para-3'>(904) 443-0343</p>
+
+
+
+            <h3 className='more'>More</h3>
+            <p className='more-details-1'>About Us</p>
+            <p className='more-details-2'>Products</p>
+            <p className='more-details-3'>Career</p>
+            <p className='more-details-4'>Contact Us</p>
+
+
+
+
+            <h3 className='social-links'>Social Links</h3>
+            <img  src="images\Path 1.png" className="social-details-1" ></img>
+            <img src='images\Path 2.png' className="social-details-2"></img>
+            <img src='images\Path 3.png' className="social-details-3"></img>
+            <p className='social-details-4'>© 2022 Food Truck Example</p>
+          
+          </div>
+        
+    
+   
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/product.js
@@ -0,0 +1,34 @@
+export const products=[{
+    
+    title: "Grilled Tomatoes at Home",
+    description: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+    img:"images\grilled-tomatoes-1-846x846 1 (2).png"
+
+},
+{
+    title: "Snacks for travel",
+    description: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+    img:"images\meal-prep-ideas-846x846 1 (4).png"   
+},
+{
+    title: "Post-workout Recipes",
+    description: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+    img:"images\meal-prep-ideas-846x846 1 (5).png"   
+},
+{
+    title: "How To Grill Corn",
+    description: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+    img:"images\grilled-tomatoes-1-846x846 1 (3).png"   
+},
+{
+    title: "Crunchwrap Supreme",
+    description: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+    img:"images\meal-prep-ideas-846x846 1 (4).png"   
+},
+{
+    title: "Broccoli Cheese Soup",
+    description: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+    img:"images\meal-prep-ideas-846x846 1 (6).png"   
+}
+
+]
